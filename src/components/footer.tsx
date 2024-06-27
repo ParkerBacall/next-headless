616a253e7aec0f16d641a7781bfd6dcd836aa111
Parker <pbacall2@gmail.com>
2024-06-27 13:13:45 -0700

    add PDP

---
+++ src/components/footer.tsx
@@ -0,0 +1,10 @@
+
+const Footer = () => {
+    return (
+      <footer className="bg-blue-600 mt-auto py-3 text-center text-white">
+        Shopify + Next.js 14!
+      </footer>
+    );
+  };
+  
+  export default Footer;
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,43 +1,45 @@
-
 export type GraphQLResponse = {
-    data: {
-      products: {
-        nodes: ShopifyProduct[];
-      };
+  data: {
+    products: {
+      nodes: ShopifyProduct[];
     };
-    extensions: ShopifyExtension;
   };
+  extensions: ShopifyExtension;
+};
 
-type ShopifyExtension = {
-    cost: {
-      actualQueryCost: number;
-      requestedQueryCost: number;
-      throttleStatus: {
-        currentlyAvailable: number;
-        maximumAvailable: number;
-        restoreRate: number;
-      };
+export type ShopifyExtension = {
+  cost: {
+    actualQueryCost: number;
+    requestedQueryCost: number;
+    throttleStatus: {
+      currentlyAvailable: number;
+      maximumAvailable: number;
+      restoreRate: number;
     };
   };
-  
-type ShopifyProduct = {
-    description: string;
-    featuredImage: {
-      altText: string;
-      height: number;
+};
+
+export type ShopifyProduct = {
+  data: {
+    product: {
+      description: string;
+      featuredImage: {
+        altText: string;
+        height: number;
+        id: string;
+        url: string;
+        width: number;
+      };
+      handle: string;
       id: string;
-      url: string;
-      width: number;
-    };
-    handle: string;
-    id: string;
-    priceRangeV2: {
-      minVariantPrice: {
-        amount: string;
-        currencyCode: string;
+      priceRangeV2: {
+        minVariantPrice: {
+          amount: string;
+          currencyCode: string;
+        };
       };
+      tags: string[];
+      title: string;
     };
-    tags: string[];
-    title: string;
   };
-  
+};
